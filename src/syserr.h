647d080d22f44c4a5e334386350a69e97a88a54c
Sevan Janiyan <venture37@geeklan.co.uk>
2015-01-24 02:40:40 +0000

    The definition of log_info() is no longer required Anything which used it has been switched to syslog(3) LOG_INFO
    	modified:   src/syserr.h

--- src/syserr.h
+++ src/syserr.h
@@ -30,7 +30,6 @@ void sys_errpack(int pri, char *fn, int ln, int en, struct sockaddr_in *peer,
 #define log_dbg(fmt,args...)    if (_options.debug) {\
                                 sys_err(LOG_DEBUG,   __FILE__,__LINE__,0,fmt,## args); }
 #define log_warn(e,fmt,args...) sys_err(LOG_WARNING, __FILE__,__LINE__,e,fmt,## args)
-#define log_info(fmt,args...)   sys_err(LOG_NOTICE,  __FILE__,__LINE__,0,fmt,## args)
 #define log_err(e,fmt,args...)  sys_err(LOG_ERR,     __FILE__,__LINE__,e,fmt,## args)
 
 #endif	/* !_SYSERR_H */
